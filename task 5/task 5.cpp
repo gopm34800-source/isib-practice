07ce2bde7f2d136b24a735a5bd609a9c058f3e27
Илья Петров <gopm34800@gmail.com>
2026-06-23 16:16:10 +0800

    ввод для 5 задачи

---
+++ task 5/task 5.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int h,w;
+    cin>>h>>w;
+    vector<vector<int>> arr(h,vector<int>(w));
+    for(int i=0;i<h;++i){
+        for(int j=0;j<w;++j){
+            cin>>arr[i][j];
+        }
+    }
+    int min_row=55,min_col=55;
+    int max_row=-1,max_col =-1;
+}
