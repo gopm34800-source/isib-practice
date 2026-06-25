876aece9ab63d5817082a3e453fbb477508dedf5
Илья Петров <gopm34800@gmail.com>
2026-06-25 21:16:22 +0800

    добавил радугу

--- task 8/sketch.ino
+++ task 8/sketch.ino
@@ -13,19 +13,28 @@ void setup() {
 }
 
 void loop() {
-  //красная змейка
-  for(int i=0;i<leds;++i) {
-    line.setPixelColor(i,255,0,0);
+  // 1) Красная змейка
+  for(int i=0; i<leds; ++i) {
+    line.setPixelColor(i, 255, 0, 0);
     line.show();                    
     delay(70);                      
   }
 
-  //  все зеленое
-  for(int i=0;i<leds;++i) {
-    line.setPixelColor(i,0,255,0);
+  // 2) Все зеленое
+  for(int i=0; i<leds; ++i) {
+    line.setPixelColor(i, 0, 255, 0);
   }
   line.show();
   delay(1000);
+
+  for(long c=0;c<5*65536;c+=256) {
+    for(int i=0;i<leds;++i) {
+      int color=c+(i*65536L/leds);
+      line.setPixelColor(i,line.gamma32(line.ColorHSV(color)));
+    }
+    line.show();
+    delay(10); 
+  }
 
   line.clear();
   line.show();
