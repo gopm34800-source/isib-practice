5940d3d1e187e20cd17ba16663e6312cb7563398
Илья Петров <gopm34800@gmail.com>
2026-06-26 20:29:20 +0800

    поиск самой большой фигуры

---
+++ task 11/task 11.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+image=cv2.imread("test.jpg")
+
+gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+ret,mask=cv2.threshold(gray,240,255,cv2.THRESH_BINARY_INV)
+
+contours,hierarchy=cv2.findContours(mask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+sorted_contours=sorted(contours,key=cv2.contourArea,reverse=True)
+
+largest_contour=sorted_contours[0]
